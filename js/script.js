63debdeb492c88bd40961deafa40cb6359c4d5ae
David Robles <davidrobles@ccpaedu.com>
2019-03-08 02:23:40 +0000

    first day

--- js/script.js
+++ js/script.js
@@ -2,10 +2,18 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var custom_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 
 $("#search-button").click(function(){
-  
-  
+    $.ajax({
+        url: custom_api_url,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            var pic_url = response.data[0].images.original.url;
+            $(".text-center").html("<img src = '" + pic_url + "'/>");
+        }
+    })
   
 });
 
