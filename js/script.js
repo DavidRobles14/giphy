7d2aa83032857f6d9d4a081d9c1639f3c4991e4a
David Robles <davidrobles@ccpaedu.com>
2019-03-22 00:55:52 +0000

    final day

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 $("#search-button").click(function(){
     var result = $("#search-term").val();
-    var custom_api_url = "https://api.giphy.com/v1/gifs/search?q=" + result + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    var custom_api_url = "https://api.giphy.com/v1/gifs/search?q=" + result + "&rating=r&api_key=dc6zaTOxFJmzC";
     $.ajax({
         url: custom_api_url,
         method: "GET",
@@ -15,9 +15,36 @@ $("#search-button").click(function(){
             console.log(response);
             var pic_url = response.data[0].images.original.url;
             $(".text-center").html("");
-            $("#img").append("<img src = '" + pic_url + "'/>");
+            $(".img").append("<img src = '" + pic_url + "'/>");
         }
     });
-  
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    
+    // Get the button that opens the modal
+    var btn = document.getElementsByClassName("img")[0];
+    console.log(btn);
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "flex";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 });
+
+
 
